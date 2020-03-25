075962652ee270eaabc4b9fd5f157290eca7e157
36const <36const@gmail.com>
2020-03-25 17:25:16 +0200

    empty strings 2

--- tests/api/GETGomerSyncSourceId/data.php
+++ tests/api/GETGomerSyncSourceId/data.php
@@ -5,20 +5,15 @@ return [
         'setting' => [
             'description' => 'Проверка заявки в статусе Ожидает проверки условий',
         ],
-
         'fixture_data' => include __DIR__ . '/fixture/case1.php',
-
         'provider_data' => [
-
             "responseCode" => 200,
-
             "responseBody" => [
                 "status" => 200,
                 "result" => [
                     "id" => 614
                 ]
-            ],
-
+            ]
         ]
     ],
 
@@ -26,20 +21,15 @@ return [
         'setting' => [
             'description' => 'Проверка заявки в статусе На проверке',
         ],
-
         'fixture_data' => include __DIR__ . '/fixture/case2.php',
-
         'provider_data' => [
-
             "responseCode" => 200,
-
             "responseBody" => [
                 "status" => 200,
                 "result" => [
                     "id" => 614
                 ]
-            ],
-
+            ]
         ]
     ],
 
@@ -47,20 +37,15 @@ return [
         'setting' => [
             'description' => 'Проверка заявки в статусе Ожидает взятия в работу',
         ],
-
         'fixture_data' => include __DIR__ . '/fixture/case3.php',
-
         'provider_data' => [
-
             "responseCode" => 200,
-
             "responseBody" => [
                 "status" => 200,
                 "result" => [
                     "id" => 614
                 ]
-            ],
-
+            ]
         ]
     ]
 
--- tests/api/PUTGomer/data.php
+++ tests/api/PUTGomer/data.php
@@ -5,25 +5,19 @@ return [
         'setting' => [
             'description' => 'Изменение количества товаров в заявке',
         ],
-
         'fixture_data' => include __DIR__ . '/fixture/case1.php',
-
         'provider_data' => [
-
             "responseCode" => 200,
-
             "responseBody" => [
                 "status" => 200,
                 "result" => [
                     "id" => 596
                 ]
             ],
-
             'db' => [
                 "requests" => [
                         "amount_to_work" => 20,
                     ],
-
             ]
         ]
     ]
