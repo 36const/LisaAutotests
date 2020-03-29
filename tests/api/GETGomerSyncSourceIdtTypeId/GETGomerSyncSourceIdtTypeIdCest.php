17cb075cacb3ffd6cdb1ef06be49b02550caaf77
36const <36const@gmail.com>
2020-03-29 15:43:53 +0300

    update get with type id

--- tests/api/GETGomerSyncSourceIdtTypeId/GETGomerSyncSourceIdtTypeIdCest.php
+++ tests/api/GETGomerSyncSourceIdtTypeId/GETGomerSyncSourceIdtTypeIdCest.php
@@ -6,10 +6,10 @@ use rzk\TestHelper;
 
 /**
  * @group lisa
- * @group GETGomerSyncSourceId
+ * @group GETGomerSyncSourceIdtTypeId
  */
 
-class GETGomerSyncSourceIdCest
+class GETGomerSyncSourceIdtTypeIdCest
 {
     /**
      * @var TestHelper $testHelper
@@ -57,16 +57,13 @@ class GETGomerSyncSourceIdCest
      * @dataProvider pageProvider
      *
      */
-    public function GETGomerSyncSourceId(ApiTester $I, \Codeception\Example $data)
+    public function GETGomerSyncSourceIdtTypeId(ApiTester $I, \Codeception\Example $data)
     {
         $providerData = $data['provider_data'];
         $this->testHelper->loadFixture($I, $data);
         $I->wantTo($data['setting']['description']);
 
-        $I->sendGET('/bpm/api/get-request-by-sync-source-id',
-            [
-                "sync_source_id" => 1,
-            ]);
+        $I->sendGET('/bpm/api/get-request-by-sync-source-id', $providerData['requestParameters']);
 
         $I->seeResponseCodeIs($providerData['responseCode']);
         $I->seeResponseContainsJson($providerData['responseBody']);
