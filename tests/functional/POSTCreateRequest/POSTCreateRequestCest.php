d1379d6280171b312a83a97659c1f9b161364bbf
36const <36const@gmail.com>
2020-04-10 09:49:00 +0300

    correcting pageProvider() and annotations, new creating test-cases

--- tests/functional/POSTCreateRequest/POSTCreateRequestCest.php
+++ tests/functional/POSTCreateRequest/POSTCreateRequestCest.php
@@ -42,7 +42,7 @@ class POSTCreateRequestCest
      */
     protected function pageProvider()
     {
-        return $this->testHelper->getDataProvider('case1');
+        return $this->testHelper->getDataProvider();
     }
 
     public function _before(FunctionalTester $I)
