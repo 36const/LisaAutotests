ab89fe931dbb7bc9b65ae69aa6de06ad1cee15e3
36const <36const@gmail.com>
2020-04-21 14:01:18 +0300

    add manager motivation tests

---
+++ tests/functional/POSTMotivation/Manager/fixture/case16.php
@@ -0,0 +1,78 @@
+<?php
+return [
+    "lisa_fixtures" => [
+        "requests" => [
+            [
+                "id" => 1,
+                "author_id" => 4,
+                "type_id" => 13,
+                "supervisor_id" => 35,
+                "manager_id" => 11,
+                "status" => 6,
+                "direction" => 0,
+                "priority" => 2,
+                "awaiting_correction" => 0,
+                "created_at" => '2020-01-01 00:00:00',
+                "correction_comment" => '',
+                "amount_to_work" => 10,
+                "subject" => "Добавление/изменение видео (Определяется типом задачи)",
+                "description" => 'description',
+                "attachments" => '[]',
+                "category_id" => 1,
+                "seller_id" => 83,
+                "recommendations" => '',
+                "reason_id" => null,
+                "reason" => null,
+                "parent_id" => null,
+                "planned_start_date" => null,
+                "planned_finish_date" => null,
+                "actual_start_date" => '2020-01-01 00:00:03',
+                "actual_finish_date" => null,
+                "supervisor_process_date" => '2020-01-01 00:00:02',
+                "supervisor_check_date" => null,
+                "difficulty" => null,
+                "result_comment" => '',
+                "supervisor_comment" => '',
+                "last_change_status_date" => "2020-01-01 00:00:01",
+                "team_direction" => 6,
+                "report_period_id" => null,
+                "sync_source_id" => null,
+                "sv_report_periods" => '{"1": 1}',
+            ],
+        ],
+        'requests_fields' => [
+            [
+                'request_id' => 1,
+                'field_id' => 1,
+                'value' => 1,
+            ],
+            [
+                'request_id' => 1,
+                'field_id' => 53,
+                'value' => 1,
+            ],
+            [
+                'request_id' => 1,
+                'field_id' => 54,
+                'value' => 1,
+            ],
+            [
+                'request_id' => 1,
+                'field_id' => 56,
+                'value' => 0,
+            ],
+            [
+                'request_id' => 1,
+                'field_id' => 101,
+                'value' => null,
+            ],
+            [
+                'request_id' => 1,
+                'field_id' => 122,
+                'value' => null,
+            ],
+        ],
+        "report_periods" => [[]],
+        "request_errors" => [[]],
+    ]
+];
