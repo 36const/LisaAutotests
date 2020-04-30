053c3d21bf7527f6b532db1330e2428113c3d7a9
36const <36const@gmail.com>
2020-04-30 10:34:56 +0300

    checking SV-motivation for all types

--- tests/_support/Page/Functional/RequestCorrection.php
+++ tests/_support/Page/Functional/RequestCorrection.php
@@ -13,6 +13,33 @@ class RequestCorrection extends FunctionalTester
     }
 
     /**
+     * Поля мотивации СВ, не отображаемые на странице correction
+     */
+    public $unchekingSVFields = [
+        '_csrf-backend',
+        //типы 1, 2, 3, 5, 6
+        'RequestField[121]',
+        'RequestField[65]',
+        //тип 4
+        'RequestField[103]',
+        'RequestField[95]',
+        //типы 9, 10, 11
+        'RequestField[104]',
+        'RequestField[105]',
+        'RequestField[106]',
+        //типы 7, 8
+        'RequestField[107]',
+        'RequestField[109]',
+        //тип 13
+        'RequestField[102]',
+        //тип 14
+        'RequestField[108]',
+        'RequestField[120]',
+        //тип 15
+        'RequestField[117]'
+    ];
+
+    /**
      * Проверка html-полей и их значений в форме ошибок для менеджера
      * @param $requestBody
      */
@@ -23,7 +50,7 @@ class RequestCorrection extends FunctionalTester
 
         foreach ($requestBody as $field => $value) {
             try {
-                ($value == '') || in_array($field, ['_csrf-backend', 'RequestField[121]', 'RequestField[65]']) ?:
+                ($value == '') || in_array($field, $unchekingSVFields) ?:
                     $I->seeElement('//div[@class="kv-attribute"][1]//*', ['name' => $field, 'value' => $value]);
             } catch (\Exception $exception) {
                 $errors[] = [
