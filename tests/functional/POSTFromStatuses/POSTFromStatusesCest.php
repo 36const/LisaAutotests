4cb14c3af0b55d716cd02cc3b340330505ce70b8
36const <36const@gmail.com>
2020-05-06 17:19:02 +0300

    little fix

--- tests/functional/POSTFromStatuses/POSTFromStatusesCest.php
+++ tests/functional/POSTFromStatuses/POSTFromStatusesCest.php
@@ -47,7 +47,7 @@ class POSTFromStatusesCest
      */
     protected function pageProvider()
     {
-        return $this->testHelper->getDataProvider('case8_6 case8_11 case9_11');
+        return $this->testHelper->getDataProvider('');
     }
 
     public function _before(FunctionalTester $I)
