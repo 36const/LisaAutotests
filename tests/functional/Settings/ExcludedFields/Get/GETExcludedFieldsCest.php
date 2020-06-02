6063dd1d66e66145efab05c7eaa82e77c45964ce
36const <36const@gmail.com>
2020-06-02 11:52:07 +0300

    get excluded-fields tests

---
+++ tests/functional/Settings/ExcludedFields/Get/GETExcludedFieldsCest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace lisa;
+
+use Codeception\Example;
+use rzk\TestHelper;
+use lisa\Page\Functional\Login;
+
+/**
+ * @group lisa
+ * @group lisa_functional
+ * @group GETExcludedFields
+ */
+class GETExcludedFieldsCest
+{
+    /**
+     * @var TestHelper $testHelper
+     */
+    private $testHelper;
+
+    /**
+     * GetMainCest constructor.
+     * Конструктор класса GetMainCest
+     *
+     * TestHelper через него идет создание фикстур моков, обработка файла data.php,
+     * возможность очистки кеша вашего приложения
+     * (Очистка кеша работает не из коробки, возможно вам нужна будет индивидуальная настройка данной функции)
+     *
+     */
+    public function __construct()
+    {
+        $this->testHelper = new TestHelper(__DIR__);
+    }
+
+    /**
+     * GETItemsFinish dataProvider
+     * Дата провайдер теста GETItemsFinish
+     *
+     * В данной функции реализуется дата провайдер который возвращает все кейсы с data.php
+     * Далее функция GETItemsFinish обрабатывает каждый кейс
+     *
+     * @return array
+     */
+    protected function pageProvider()
+    {
+        return $this->testHelper->getDataProvider('');
+    }
+
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    /**
+     * @param FunctionalTester $I
+     * @param Example $data
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @dataProvider pageProvider
+     *
+     */
+    public function GETExcludedFields(FunctionalTester $I, Example $data, Login $login)
+    {
+        $I->loadDataForTest($data, $this->testHelper);
+        $providerData = $data['provider_data'];
+
+        $login->login();
+
+        $I->amOnPage('bpm/excluded-fields/index/' . $providerData['url']);
+        $I->seeResponseCodeIs(200);
+
+        $I->checkFieldsOnPage($providerData['pageObjects']);
+    }
+}
