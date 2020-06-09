ca4d5c525cb66a73f679ad0c4034327dded351e3
36const <36const@gmail.com>
2020-06-09 09:24:54 +0300

    POSTRelatedRequestCest

---
+++ tests/functional/Requests/Related/POSTRelatedRequestCest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace lisa;
+
+use Codeception\Example;
+use rzk\TestHelper;
+use lisa\Page\Functional\Login;
+use lisa\Page\Functional\RequestCreate;
+use lisa\Page\Functional\RequestView;
+
+/**
+ * @group lisa
+ * @group lisa_functional
+ * @group lisa_functional_requests
+ * @group POSTRelatedRequest
+ */
+class POSTRelatedRequestCest
+{
+    /**
+     * @var TestHelper $testHelper
+     */
+    private $testHelper;
+
+    /**
+     * GetMainCest constructor.
+     * Конструктор класса GetMainCest
+     *
+     * TestHelper через него идет создание фикстур моков, обработка файла data.php,
+     * возможность очистки кеша вашего приложения
+     * (Очистка кеша работает не из коробки, возможно вам нужна будет индивидуальная настройка данной функции)
+     *
+     */
+    public function __construct()
+    {
+        $this->testHelper = new TestHelper(__DIR__);
+    }
+
+    /**
+     * GETItemsFinish dataProvider
+     * Дата провайдер теста GETItemsFinish
+     *
+     * В данной функции реализуется дата провайдер который возвращает все кейсы с data.php
+     * Далее функция GETItemsFinish обрабатывает каждый кейс
+     *
+     * @return array
+     */
+    protected function pageProvider()
+    {
+        return $this->testHelper->getDataProvider('');
+    }
+
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    /**
+     * @param FunctionalTester $I
+     * @param Example $data
+     * @param Login $login
+     * @param RequestCreate $create
+     * @param RequestView $view
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @dataProvider pageProvider
+     *
+     */
+    public function POSTRelatedRequest(FunctionalTester $I, Example $data, Login $login,
+                                       RequestCreate $create, RequestView $view)
+    {
+        $I->loadDataForTest($data, $this->testHelper);
+
+        $setting = $data['setting'];
+        $providerData = $data['provider_data'];
+
+        $providerData['requestBody']['_csrf-backend'] = $login->login();
+        $create->checkFields($providerData['db'], $setting['type'], $setting['direction'], $setting['id']);
+
+        $I->sendPOST('/bpm/request/create', $providerData['requestBody']);
+        $I->seeResponseCodeIs(200);
+
+        $I->checkTablesInDB($providerData['db']);
+        $view->checkFields($providerData['db']);
+    }
+}
---
+++ tests/functional/Requests/Related/fixture/case6.php
@@ -0,0 +1,143 @@
+<?php
+
+return [
+    "lisa_fixtures" => [
+        "requests" => [
+            [
+//                "id" => 1,
+                "author_id" => 4,
+                "type_id" => 13,
+                "supervisor_id" => 38,
+                "manager_id" => 11,
+                "status" => 8,
+                "direction" => 0,
+                "priority" => 2,
+                "awaiting_correction" => 0,
+                "created_at" => '2020-01-01 00:00:00',
+                "correction_comment" => null,
+                "amount_to_work" => 10,
+                "subject" => "Добавление/изменение видео (Определяется типом задачи)",
+                "description" => "Описание",
+                "attachments" => "[]",
+                "category_id" => 1,
+                "seller_id" => 83,
+                "recommendations" => null,
+                "reason_id" => 6,
+                "reason" => null,
+                "parent_id" => null,
+                "planned_start_date" => null,
+                "planned_finish_date" => null,
+                "actual_start_date" => '2020-01-01 00:00:03',
+                "actual_finish_date" => '2020-01-01 00:00:04',
+                "supervisor_process_date" => '2020-01-01 00:00:02',
+                "supervisor_check_date" => null,
+                "difficulty" => null,
+                "result_comment" => "Комментарий к результату задачи",
+                "supervisor_comment" => 'Комментарий',
+                "last_change_status_date" => "2020-01-01 00:00:01",
+                "team_direction" => 6,
+                "report_period_id" => null,
+                "sync_source_id" => null,
+                "sv_report_periods" => '{"1": 1}'
+            ],
+        ],
+        "requests_fields" => [
+            [
+                "request_id" => 1,
+                "field_id" => 2,
+                "value" => "1"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 20,
+                "value" => "66"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 27,
+                "value" => "24"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 47,
+                "value" => "10"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 48,
+                "value" => "10"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 53,
+                "value" => "2"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 54,
+                "value" => "2"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 56,
+                "value" => "80"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 62,
+                "value" => "10"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 63,
+                "value" => "10"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 70,
+                "value" => "10"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 86,
+                "value" => "10"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 87,
+                "value" => "10"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 88,
+                "value" => "10"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 100,
+                "value" => "10"
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 101,
+                "value" => null
+            ],
+            [
+                "request_id" => 1,
+                "field_id" => 122,
+                "value" => null
+            ],
+        ],
+        "report_periods" => [[]],
+        "request_errors" => [],
+        'notification_settings' => [],
+        "user_notifications" => [],
+        "request_status_history" => [],
+        "observers" => [],
+        "categories" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/categories.php',
+        "markets_supervisors" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/markets_supervisors.php',
+        "field_values" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/field_values.php',
+        "difficulty_coefs" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/difficulty_coefs.php',
+        "teams" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/teams.php'
+    ],
+];
