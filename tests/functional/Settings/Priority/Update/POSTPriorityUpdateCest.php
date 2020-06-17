c3aa54efe089218fb7633fcc7f8950fbdc5787d7
36const <36const@gmail.com>
2020-06-17 16:36:10 +0300

    new Priorities tests

---
+++ tests/functional/Settings/Priority/Update/POSTPriorityUpdateCest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace lisa;
+
+use Codeception\Example;
+use rzk\TestHelper;
+use lisa\Page\Functional\Login;
+use lisa\Page\Functional\RequestView;
+
+/**
+ * @group lisa
+ * @group lisa_functional
+ * @group lisa_functional_settings
+ * @group POSTPriority
+ * @group POSTPriorityUpdate
+ */
+class POSTPriorityUpdateCest
+{
+    /**
+     * @var TestHelper $testHelper
+     */
+    private $testHelper;
+
+    public function __construct()
+    {
+        $this->testHelper = new TestHelper(__DIR__);
+    }
+
+    /**
+     * @return array
+     */
+    protected function pageProvider()
+    {
+        return $this->testHelper->getDataProvider('');
+    }
+
+    /**
+     * @param FunctionalTester $I
+     * @param Example $data
+     * @param Login $login
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @dataProvider pageProvider
+     *
+     */
+    public function POSTPriorityUpdateCest(FunctionalTester $I, Example $data, Login $login)
+    {
+        $I->loadDataForTest($data, $this->testHelper);
+
+        $providerData = $data['provider_data'];
+
+        $providerData['requestBody']['_csrf-backend'] = $login->login();
+
+        $I->sendPOST('/bpm/priority/update?id=1', $providerData['requestBody']);
+        $I->seeResponseCodeIs(200);
+
+        $I->checkTablesInDB($providerData['db']);
+    }
+}
---
+++ tests/functional/Settings/Priority/Update/fixture/case1.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "lisa_fixtures" => [
+        "priorities" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/priorities.php'
+    ],
+];
