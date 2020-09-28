c4883cdb7443d262ac3348e7c2f9e3761830f8bb
kutsan.k <kutsan.k@rozetka.com.ua>
2020-09-28 09:47:17 +0300

    acceptance

---
+++ tests/_support/AcceptanceTester.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace lisa;
+
+use Codeception\Example;
+use rzk\TestHelper;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method void pause()
+ *
+ * @SuppressWarnings(PHPMD)
+ */
+class AcceptanceTester extends \Codeception\Actor
+{
+    use _generated\FunctionalTesterActions;
+
+    /**
+     * @param Example $data - данные кейса из файла data.php
+     * @param TestHelper $testHelper
+     * @param array|string[] $globalFile - название файла глобальных фикстур
+     * @param bool $globalUsing - нужно ли использовать глобальные фикстуры
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function loadDataForTest(Example $data, TestHelper $testHelper,
+                                    array $globalFile = ['oneUser'], bool $globalUsing = true)
+    {
+        $I = $this;
+        $I->runShellCommand('./yii bpm/request/clear-lisa-redis');
+        $I->runShellCommand('./yii bpm/request/clear-temporary-files');
+        $I->purgeAllQueues();
+        $testHelper->clearDB($I, $data, $globalFile);
+
+        if ($globalUsing)
+            $testHelper->loadGlobalFixture($I, $globalFile);
+
+        $testHelper->loadFixtureAndMock($I, $data);
+
+        $I->wantTo($data['setting']['description']);
+        $I->amOnPage('/');
+    }
+
+    public function changeStatus($requestParameter, $requestBody)
+    {
+        $I = $this;
+        $requestParameter == 'to-correction' ?
+            $url = '/bpm/request/' . "$requestParameter" . '?id=1&changeStatus=1' :
+            $url = '/bpm/request/' . "$requestParameter" . '?id=1';
+        $I->sendPOST($url, $requestBody);
+        $I->seeResponseCodeIs(200);
+    }
+
+    public function changeType($requestParameter, $requestBody)
+    {
+        $I = $this;
+        $url = '/bpm/request/change-type?typeId=' . $requestParameter['typeId'] . '&direction=' . $requestParameter['direction'] . '&id=1';
+        $I->sendPOST($url, $requestBody);
+        $I->seeResponseCodeIs(200);
+    }
+
+    public function checkTablesInDB($dbTablesArray, bool $dontSee = false)
+    {
+        $I = $this;
+
+        foreach ($dbTablesArray as $dbName => $dbData) {
+            $I->amConnectedToDatabase($dbName);
+
+            foreach ($dbData as $tableName => $tableData) {
+
+                $expectedNumber = count($tableData);
+
+                ($dontSee) ?:
+                    $I->canSeeNumRecords($expectedNumber, $tableName);
+
+                foreach ($tableData as $tableRow) {
+                    (!$dontSee) ?
+                        $I->canSeeInDatabase($tableName, $tableRow) :
+                        $I->cantSeeInDatabase($tableName, $tableRow);
+                }
+            }
+        }
+    }
+
+    public function checkObjectsOnPage($pageObjects)
+    {
+        $I = $this;
+
+        if (isset($pageObjects['canSee'])) {
+            foreach ($pageObjects['canSee'] as $objects) {
+                foreach ($objects as $object) {
+                    isset($object['value']) ?
+                        $I->canSee($object['value'], $object['selector']) :
+                        $I->canSeeElement($object['selector']);
+                }
+            }
+        }
+
+        if (isset($pageObjects['cantSee'])) {
+            foreach ($pageObjects['cantSee'] as $objects) {
+                foreach ($objects as $object) {
+                    isset($object['value']) ?
+                        $I->cantSee($object['value'], $object['selector']) :
+                        $I->cantSeeElement($object['selector']);
+                }
+            }
+        }
+    }
+}
---
+++ tests/acceptance/Requests/Create/fixture/case1.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    "lisa_fixtures" => [
+        "requests" => [],
+        "requests_fields" => [],
+        "request_errors" => [],
+        'notification_settings' => [],
+        "user_notifications" => [],
+        "request_status_history" => [],
+        "observers" => [],
+        "categories" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/categories.php',
+        "markets_supervisors" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/markets_supervisors.php',
+        "field_values" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/field_values.php',
+        "difficulty_coefs" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/difficulty_coefs.php',
+        "teams" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/teams.php',
+        "priorities" => include \rzk\FixtureController::getFixtureDir() . 'lisa_fixtures/priorities.php',
+    ],
+    "cooper_fixtures" => [
+        "market" => include \rzk\FixtureController::getFixtureDir() . 'cooper_fixtures/market.php',
+    ]
+];
