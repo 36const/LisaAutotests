0d39ec69455ece957d0096524bad6fd1d1b59bce
kutsan.k <kutsan.k@rozetka.com.ua>
2020-10-28 18:20:43 +0200

    372

---
+++ tests/api/Comi/POSTComi/POSTComiCest.php
@@ -0,0 +1,52 @@
+<?php
+namespace lisa;
+
+use Codeception\Example;
+use rzk\TestHelper;
+
+/**
+ * @group lisa
+ * @group lisa_api
+ * @group POSTMobileCurrier
+ */
+class POSTComiCest
+{
+    /**
+     * @var TestHelper $testHelper
+     */
+    private $testHelper;
+
+    public function __construct()
+    {
+        $this->testHelper = new TestHelper(__DIR__);
+    }
+
+    /**
+     * @return array
+     */
+    protected function pageProvider()
+    {
+        return $this->testHelper->getDataProvider('');
+    }
+
+    /**
+     * @param ApiTester $I
+     * @param Example $data
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @dataProvider pageProvider
+     *
+     */
+    public function POSTMobileCurrier(ApiTester $I, Example $data)
+    {
+        $I->loadDataForTest($data, $this->testHelper, ['allUsers']);
+        $providerData = $data['provider_data'];
+
+        $I->sendPOST('/bpm/api/create-request-by-mobile-courier', $providerData['requestBody']);
+
+        $I->seeResponseCodeIs($providerData['responseCode']);
+        $I->seeResponseContainsJson($providerData['responseBody']);
+
+        $I->checkTablesInDB($providerData['db']);
+    }
+}
