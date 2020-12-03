a227633c827bd28c13435e31ce3a6674b217721b
kutsan.k <kutsan.k@rozetka.com.ua>
2020-12-03 11:50:00 +0200

    checkRabbitMQ

--- tests/functional/Requests/Create/Post/fixture/case1.php
+++ tests/functional/Requests/Create/Post/fixture/case1.php
@@ -22,7 +22,7 @@ return [
             [
                 //'id' => 1,
                 "user_id" => 15,
-                "types_for_notification" => '{"create": 2, "comment": 1, "mention": "1", "manager_id": 1, "attachments": 1, "supervisor_id": 1}',
+                "types_for_notification" => '{"create": 2, "comment": 2, "mention": "1", "manager_id": 1, "attachments": 1, "supervisor_id": 1}',
                 "fields_for_complete" => '["1", "2", "20", "29", "30", "53", "54", "67", "101", "122"]',
                 "statuses_for_notification" => '{"1": 2, "2": 2, "3": 2, "4": 2, "5": 2, "6": 2, "7": 2, "8": 2, "9": 2, "11": 2}'
             ],
@@ -31,7 +31,7 @@ return [
                 "user_id" => 17,
                 "types_for_notification" => '{"create": 2, "comment": 2, "mention": "2", "manager_id": 2, "attachments": 2, "supervisor_id": 2}',
                 "fields_for_complete" => '["1", "2", "20", "29", "30", "53", "54", "67", "101", "122"]',
-                "statuses_for_notification" => '{"1": 1, "2": 1, "3": 1, "4": 1, "5": 1, "6": 1, "7": 1, "8": 1, "9": 1, "11": 1}'
+                "statuses_for_notification" => '{"1": 1, "2": 1, "3": 2, "4": 2, "5": 1, "6": 1, "7": 1, "8": 1, "9": 1, "11": 1}'
             ],
         ],
         "user_notifications" => [],
