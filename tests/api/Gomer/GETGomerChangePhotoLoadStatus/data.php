94270418e948ff1bc53627b38e0d11fd2bd1c1e3
kutsan.k <kutsan.k@rozetka.com.ua>
2021-01-06 10:56:28 +0200

    GETGomerChangePhotoLoadStatus

--- tests/api/Gomer/GETGomerChangePhotoLoadStatus/data.php
+++ tests/api/Gomer/GETGomerChangePhotoLoadStatus/data.php
@@ -795,6 +795,75 @@ return [
         ]
     ],
 
+    'case8_1_2' => [
+        'setting' => [
+            'description' => 'Заявка тип 1 направление 2 статус 7 ризоны 1 и 2',
+        ],
+        'fixture_data' => include __DIR__ . '/fixture/case8_1_2.php',
+        'provider_data' => [
+            "requestParameters" => [
+                "requestId" => '1'
+            ],
+            "responseCode" => 200,
+            "responseBody" => [
+                "status" => 200,
+                "message" => "Ok"
+            ],
+            'db' => [
+                'lisa_fixtures' => [
+                    "requests" => [
+                        [
+                            "id" => 1,
+                            "author_id" => 4,
+                            "type_id" => 1,
+                            "supervisor_id" => 7,
+                            "manager_id" => null,
+                            "status" => 7,
+                            "direction" => 2,
+                            "priority" => null,
+                            "awaiting_correction" => 0,
+                            "created_at" => "2020-01-01 00:00:00",
+                            "correction_comment" => null,
+                            "amount_to_work" => 10,
+                            "subject" => "Добавление новых товаров (Работа с товарами Маркета)",
+                            "description" => "description",
+                            "category_id" => null,
+                            "seller_id" => 83,
+                            "recommendations" => null,
+                            //"reason_id" => 2,
+                            "reason" => null,
+                            "parent_id" => null,
+                            "planned_start_date" => null,
+                            "planned_finish_date" => null,
+                            "actual_start_date" => null,
+                            "actual_finish_date" => null,
+                            "supervisor_process_date" => null,
+                            "supervisor_check_date" => null,
+                            "result_comment" => null,
+                            "supervisor_comment" => null,
+                            "last_change_status_date >=" => date("Y-m-d"),
+                            "team_direction" => 2,
+                            "report_period_id" => null,
+                            "sync_source_id" => null,
+                            "sv_report_periods" => null,
+                            "cross_check_status" => 0,
+                            "cross_check_manager_id" => null,
+                            "employee_code_1c" => null,
+                            "child_count" => 0,
+                            "photo_load_status" => 1
+                        ],
+                    ],
+                    'requests_reasons' => [
+                        [
+                            "request_id" => 1,
+                            "reason_id" => 2
+                        ]
+                    ]
+                ]
+            ],
+        ]
+    ],
+
     'case9' => [
         'setting' => [
             'description' => 'Заявка тип 1 направление 1 статус 8',
