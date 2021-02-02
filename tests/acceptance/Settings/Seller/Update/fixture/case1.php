0cf80371016e09ce99269d52743160fafe9a6f98
kutsan.k <kutsan.k@rozetka.com.ua>
2021-02-02 12:55:50 +0200

    sellers

--- tests/acceptance/Settings/Seller/Update/fixture/case1.php
+++ tests/acceptance/Settings/Seller/Update/fixture/case1.php
@@ -1,28 +1,7 @@
 <?php
 return [
     "lisa_fixtures" => [
-        "markets_supervisors" => [
-            [
-                "owox_id" => 0,
-                "supervisor_id" => 5
-            ],
-            [
-                "owox_id" => 5,
-                "supervisor_id" => 6
-            ],
-            [
-                "owox_id" => 83,
-                "supervisor_id" => 6
-            ],
-            [
-                "owox_id" => 9423,
-                "supervisor_id" => 10
-            ],
-            [
-                "owox_id" => 67757,
-                "supervisor_id" => 10
-            ],
-        ],
+        "markets_supervisors" => include \Codeception\Module\TestHelper::getFixtureTempleteDefaultPath() . 'lisa_fixtures/markets_supervisors.php',
     ],
     "cooper_fixtures" => [
         "market" => include \Codeception\Module\TestHelper::getFixtureTempleteDefaultPath() . 'cooper_fixtures/market.php',
