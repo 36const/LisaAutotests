38a36f89cb75e76ce3c93ebd712cac211ca1b4b7
kutsan.k <kutsan.k@rozetka.com.ua>
2021-02-03 15:21:20 +0200

    blocked field values

--- tests/functional/Settings/FieldValues/GetInRequest/data.php
+++ tests/functional/Settings/FieldValues/GetInRequest/data.php
@@ -223,4 +223,192 @@ return [
         ]
     ],
 
+    'case2' => [
+        'setting' => [
+            'description' => 'Значения выпадающих списков полей структуры и сложности в алфавитном порядке + показываются заблоченные',
+        ],
+        'fixture_data' => include __DIR__ . '/fixture/case1.php',
+        'provider_data' => [
+            "url" => 'bpm/request/view?id=1&forCrossCheck=0',
+            "pageObjects" => [
+                "canSee" => [
+                    "Вид структуры" => [
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[1][@value]",
+                            "value" => "не задано"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[2][@value='31']",
+                            "value" => "Новые категории промежуточных уровней/автопорталы"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[3][@value='32']",
+                            "value" => "Новые конечные категории"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[4][@value='30']",
+                            "value" => "Новые разделы (категории верхнего уровня, второго за верхним уровня)"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[5][@value='34']",
+                            "value" => "Существующие категории промежуточных уровней/автопорталы"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[6][@value='37']",
+                            "value" => "Существующие категории промежуточных уровней/автопорталы + перенос информации"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[7][@value='35']",
+                            "value" => "Существующие конечные категории"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[8][@value='38' and @disabled]",
+                            "value" => "Существующие конечные категории + перенос информации"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[9][@value='33']",
+                            "value" => "Существующие разделы (категории верхнего уровня, второго за верхним уровня)"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[29]']/option[10][@value='36']",
+                            "value" => "Существующие разделы (категории верхнего уровня, второго за верхним уровня) + перенос информации"
+                        ],
+                    ],
+                    "Сложность структуры" => [
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[1][@value]",
+                            "value" => "не задано"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[2][@value='39']",
+                            "value" => "Простые (физические характеристики)"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[3][@value='40']",
+                            "value" => "Простые (физические характеристики) 1 вид товара"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[4][@value='41']",
+                            "value" => "Простые (физические характеристики) несколько видов товара"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[5][@value='45']",
+                            "value" => "Сложные (физические + функциональные + технологические характеристики)"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[6][@value='46']",
+                            "value" => "Сложные (физические + функциональные + технологические характеристики) 1 вид товара"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[7][@value='47' and @disabled]",
+                            "value" => "Сложные (физические + функциональные + технологические характеристики) несколько видов товара"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[8][@value='42']",
+                            "value" => "Средней сложности (физические + функциональные характеристики)"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[9][@value='43']",
+                            "value" => "Средней сложности (физические + функциональные характеристики) 1 вид товара"
+                        ],
+                        [
+                            "selector" => "//select[@name='RequestField[30]']/option[10][@value='44']",
+                            "value" => "Средней сложности (физические + функциональные характеристики) несколько видов товара"
+                        ],
+                    ],
+                ],
+            ]
+        ]
+    ],
+
+    'case3' => [
+        'setting' => [
+            'description' => 'Значения выпадающих списков пакетного редактирования в алфавитном порядке + не показываются заблоченные',
+        ],
+        'fixture_data' => include __DIR__ . '/fixture/case1.php',
+        'provider_data' => [
+            "url" => 'bpm/request/in-check',
+            "pageObjects" => [
+                "canSee" => [
+                    "Вид структуры" => [
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[29]']/option[1][@value]",
+                            "value" => "Выберите Вид структуры..."
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[29]']/option[2][@value='31']",
+                            "value" => "Новые категории промежуточных уровней/автопорталы"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[29]']/option[3][@value='32']",
+                            "value" => "Новые конечные категории"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[29]']/option[4][@value='30']",
+                            "value" => "Новые разделы (категории верхнего уровня, второго за верхним уровня)"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[29]']/option[5][@value='34']",
+                            "value" => "Существующие категории промежуточных уровней/автопорталы"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[29]']/option[6][@value='37']",
+                            "value" => "Существующие категории промежуточных уровней/автопорталы + перенос информации"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[29]']/option[7][@value='35']",
+                            "value" => "Существующие конечные категории"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[29]']/option[8][@value='33']",
+                            "value" => "Существующие разделы (категории верхнего уровня, второго за верхним уровня)"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[29]']/option[9][@value='36']",
+                            "value" => "Существующие разделы (категории верхнего уровня, второго за верхним уровня) + перенос информации"
+                        ],
+                    ],
+                    "Сложность структуры" => [
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[30]']/option[1][@value]",
+                            "value" => "Выберите Сложность структуры..."
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[30]']/option[2][@value='39']",
+                            "value" => "Простые (физические характеристики)"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[30]']/option[3][@value='40']",
+                            "value" => "Простые (физические характеристики) 1 вид товара"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[30]']/option[4][@value='41']",
+                            "value" => "Простые (физические характеристики) несколько видов товара"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[30]']/option[5][@value='45']",
+                            "value" => "Сложные (физические + функциональные + технологические характеристики)"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[30]']/option[6][@value='46']",
+                            "value" => "Сложные (физические + функциональные + технологические характеристики) 1 вид товара"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[30]']/option[7][@value='42']",
+                            "value" => "Средней сложности (физические + функциональные характеристики)"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[30]']/option[8][@value='43']",
+                            "value" => "Средней сложности (физические + функциональные характеристики) 1 вид товара"
+                        ],
+                        [
+                            "selector" => "//form[@id='mass-edit-form']//select[@name='RequestField[30]']/option[9][@value='44']",
+                            "value" => "Средней сложности (физические + функциональные характеристики) несколько видов товара"
+                        ],
+                    ],
+                ],
+            ]
+        ]
+    ],
+
 ];
