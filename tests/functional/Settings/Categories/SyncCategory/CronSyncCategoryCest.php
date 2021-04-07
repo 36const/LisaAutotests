e342dccb80c41ee9832dde58e370cccad9a0c11e
kutsan.k <kutsan.k@rozetka.com.ua>
2021-04-07 11:10:25 +0300

    LISA RabbitMQ

---
+++ tests/functional/Settings/Categories/SyncCategory/CronSyncCategoryCest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace lisa;
+
+use Codeception\Example;
+use Codeception\Module\TestHelper;
+
+/**
+ * @group lisa
+ * @group lisa_functional
+ * @group lisa_functional_settings
+ * @group CronSyncCategory
+ */
+class CronSyncCategoryCest
+{
+    protected function pageProvider(): array
+    {
+        return TestHelper::prepareDataprovider(require 'data.php', '');
+    }
+
+    /**
+     * @param FunctionalTester $I
+     * @param Example $data
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @dataProvider pageProvider
+     */
+    public function CronSyncCategory(FunctionalTester $I, Example $data)
+    {
+        $I->loadDataForTest($data);
+        $providerData = $data['provider_data'];
+
+        $I->runShellCommand('./yii bpm/request/sync-category');
+
+        $I->declareExchange('goods_service', 'topic');
+        $I->bindQueueToExchange('lisa', 'goods_service', 'create.category.entity.DVR');
+
+        $I->pushToExchange('goods_service', $providerData['message'], 'create.category.entity.DVR');
+
+        $I->runShellCommand('./yii bpm/request/sync-category');
+        $I->canSeeResultCodeIs(0);
+
+        $I->checkTablesInDB($providerData['db']);
+        $I->checkRabbitMQ($providerData);
+    }
+}
---
+++ tests/functional/Settings/Categories/SyncCategory/fixture/case1.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "lisa_fixtures" => [
+        "rozetka_categories" => include \Codeception\Module\TestHelper::getFixtureTempleteDefaultPath() . 'lisa_fixtures/rozetka_categories.php',
+    ],
+];
