cb9cd8a0e5919441868ad30afe06d831e0978e32
kutsan.k <90K24g75>
2021-07-09 13:26:43 +0300

    excludedRedisKeys

--- tests/functional/Settings/Categories/CronSyncCategory/data.php
+++ tests/functional/Settings/Categories/CronSyncCategory/data.php
@@ -9,10 +9,6 @@ return [
         'provider_data' => [
             'message' => '{"data":{"id":4326656,"mpath":".84625.4326572.","title":"Учебники","status":"active","status_inherited":"active","order":16777215,"ext_id":"","name":"uchebniki-nauchno-metodicheskaya-literatura","titles_mode":"inherited","kits_show":"inherited","parent_id":4326572,"left_key":8044,"right_key":8045,"level":0,"created_at":"2015-08-28T10:02:42Z","href":"https://rozetka.com.ua/uchebniki-nauchno-metodicheskaya-literatura/c4326656/","allow_index_three_parameters":false,"on_subdomain":"none","oversized":"default","is_subdomain":false,"disable_kit_ratio":false,"is_rozetka_top":true,"uk":{"id":4326656,"title":""},"use_group_links":""},"changed":["mpath", "status_inherited"], "time": "2021-06-25T07:11:02.333270217Z","request_id":"MessageBus_Modules_RozetkaCLIController::sendRecordsDataByWhere"}',
             'routing_key' => 'create.category.entity.SLR.DVR.CMT.CTT.SEO.BI.LLT.1C.MRK.CTL.DBA.EBQ.GML.SMD.SSH.LIS',
-            'excludedRedisKeys' => [
-                'all_category_namesfa94761536e04215430d7de0b502b16e',
-                'active_category_ids5ec4d878ccfce7dc3c19481d6a063478'
-            ],
             'db' => [
                 "lisa_fixtures" => [
                     "rozetka_categories" => [
